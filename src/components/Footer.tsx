c66747bc9b60a4c5db846bdc9d734345eb0efe0b
laupwing <laupwing@gmail.com>
2024-11-05 19:08:45 +0100

    Refactor Footer component for improved readability and consistency; add missing keys to mapped elements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,43 +5,44 @@ import { Accent } from "./elements/Accent"
 // import { Tooltip } from "./Tooltip"
 import { UnstyledLink } from "./links/UnstyledLink.client"
 
-export const Footer:FC = () => {
-   return (
-      <footer className="pt-4 pb-12">
-         <main className="layout flex flex-col items-center border-t pt-6 dark:border-gray-600">
-            <FooterLinks />
-            <p className="mt-12 font-medium text-gray-600 dark:text-gray-300">
-               Reach Out
-            </p>
+export const Footer: FC = () => {
+    return (
+        <footer className="pt-4 pb-12">
+            <main className="layout flex flex-col items-center border-t pt-6 dark:border-gray-600">
+                <FooterLinks />
+                <p className="mt-12 font-medium text-gray-600 dark:text-gray-300">
+                    Reach Out
+                </p>
 
-            <p className="mt-8 text-sm text-gray-600 dark:text-gray-300">
-               © Laup Wing {new Date().getFullYear()}
-            </p>
-         </main>
-      </footer>
-   )
+                <p className="mt-8 text-sm text-gray-600 dark:text-gray-300">
+                    © Laup Wing {new Date().getFullYear()}
+                </p>
+            </main>
+        </footer>
+    )
 }
 
 const FooterLinks = () => {
-   return (
-      <div className="flex flex-wrap justify-center gap-x-8 gap-y-4">
-         {footerLinks.map(({ href, text }) => (
-            <UnstyledLink
-               className="animated-underline rounded-sm text-sm font-medium focus:outline-none focus-visible:ring focus-visible:ring-accent-dark dark:text-gray-200 dark:focus-visible:ring-accent-light"
-               href={href}
-            >
-               {text}
-            </UnstyledLink>
-         ))}
-      </div>
-   )
+    return (
+        <div className="flex flex-wrap justify-center gap-x-8 gap-y-4">
+            {footerLinks.map(({ href, text }) => (
+                <UnstyledLink
+                    className="animated-underline rounded-sm text-sm font-medium focus:outline-none focus-visible:ring focus-visible:ring-accent-dark dark:text-gray-200 dark:focus-visible:ring-accent-light"
+                    href={href}
+                    key={href}
+                >
+                    {text}
+                </UnstyledLink>
+            ))}
+        </div>
+    )
 }
 
 const SocialLinks = () => {
-   return (
-      <div className="mt-2 flex space-x-4">
-         <div className="flex items-center justify-center">
-            {/* <Tooltip
+    return (
+        <div className="mt-2 flex space-x-4">
+            <div className="flex items-center justify-center">
+                {/* <Tooltip
                trigger="mouseenter"
                hideOnClick={false}
                interactive
@@ -56,78 +57,80 @@ const SocialLinks = () => {
 
                </button>
             </Tooltip> */}
-         </div>
-      </div>
-   )
+            </div>
+        </div>
+    )
 }
 
 interface Social {
-   href: string
-   icon: IconType
-   id: string
-   text: ReactNode
+    href: string
+    icon: IconType
+    id: string
+    text: ReactNode
 }
 
 const social: Social[] = [
-   {
-      href: "https://github.com/LaupWing",
-      icon: IconGithub,
-      id: "Github",
-      text: (
-         <>
-            See my projects on <Accent className="font-medium">Github</Accent>
-         </>
-      )
-   },
-   {
-      href: "https://www.linkedin.com/in/loc-nguyen-a33896272/",
-      icon: IconLinkedin,
-      id: "Linkedin",
-      text: (
-         <>
-            Find me on <Accent className="font-medium">Linkedin</Accent>
-         </>
-      )
-   },
-   {
-      href: "https://twitter.com/LaupWing1994",
-      icon: IconTwitter,
-      id: "Twitter",
-      text: (
-         <>
-            For tech tips. Follow me on <Accent className="font-medium">Twitter</Accent>
-         </>
-      )
-   },
+    {
+        href: "https://github.com/LaupWing",
+        icon: IconGithub,
+        id: "Github",
+        text: (
+            <>
+                See my projects on{" "}
+                <Accent className="font-medium">Github</Accent>
+            </>
+        ),
+    },
+    {
+        href: "https://www.linkedin.com/in/loc-nguyen-a33896272/",
+        icon: IconLinkedin,
+        id: "Linkedin",
+        text: (
+            <>
+                Find me on <Accent className="font-medium">Linkedin</Accent>
+            </>
+        ),
+    },
+    {
+        href: "https://twitter.com/LaupWing1994",
+        icon: IconTwitter,
+        id: "Twitter",
+        text: (
+            <>
+                For tech tips. Follow me on{" "}
+                <Accent className="font-medium">Twitter</Accent>
+            </>
+        ),
+    },
 ]
 
 const footerLinks: Array<{
-   href: string
-   text: string
-   tooltip?: ReactNode 
+    href: string
+    text: string
+    tooltip?: ReactNode
 }> = [
-   {
-      href: "https://github.com/LaupWing/tech-blog",
-      text: "Source Code",
-   },
-   {
-      href: "/",
-      text: "Home"
-   },
-   {
-      href: "/blog",
-      text: "Blog"
-   },
-   {
-      href: "/projects",
-      text: "Projects"
-   },
-   {
-      href: "/library",
-      text: "Library"
-   },
-   {
-      href: "/about",
-      text: "About"
-   },
+    {
+        href: "https://github.com/LaupWing/tech-blog",
+        text: "Source Code",
+    },
+    {
+        href: "/",
+        text: "Home",
+    },
+    {
+        href: "/blog",
+        text: "Blog",
+    },
+    {
+        href: "/projects",
+        text: "Projects",
+    },
+    {
+        href: "/library",
+        text: "Library",
+    },
+    {
+        href: "/about",
+        text: "About",
+    },
 ]
