3d6831b1de430102fbd26516d9fbda1c8a54f663
laupwing <laupwing@gmail.com>
2024-11-12 18:07:31 +0100

    Refactor attachContentMeta function to use a for loop instead of Promise.all for improved readability and control over database connections

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -48,32 +48,36 @@ export const getFromSessionStorage = (key: string) => {
 export const attachContentMeta = async <T extends ContentType>(
     frontmatters: Array<PickFrontmatter<T>>
 ) => {
-    return await Promise.all(
-        frontmatters.map(async (frontmatter) => {
-            const data = await prisma.contentMeta.upsert({
-                where: {
-                    slug: frontmatter.slug,
-                },
-                update: {
-                    slug: frontmatter.slug,
-                },
-                create: {
-                    slug: frontmatter.slug,
-                },
-                include: {
-                    _count: {
-                        select: {
-                            Like: true,
-                            View: true,
-                        },
+    const results = []
+
+    for (const frontmatter of frontmatters) {
+        const data = await prisma.contentMeta.upsert({
+            where: {
+                slug: frontmatter.slug,
+            },
+            update: {
+                slug: frontmatter.slug,
+            },
+            create: {
+                slug: frontmatter.slug,
+            },
+            include: {
+                _count: {
+                    select: {
+                        Like: true,
+                        View: true,
                     },
                 },
-            })
-            return {
-                ...frontmatter,
-                views: data._count.View,
-                likes: data._count.Like,
-            }
+            },
         })
-    )
+
+        results.push({
+            ...frontmatter,
+            views: data._count.View,
+            likes: data._count.Like,
+        })
+        prisma.$disconnect()
+    }
+
+    return results
 }
